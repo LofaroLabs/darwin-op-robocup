38ba8bc15c3e911c6c1c5ec9a5b22971788cabcf
David <devnull@localhost>
2014-06-04 12:29:57 -0400

    we should be moving

--- UPENN2013/Player/hoard_functions.lua
+++ UPENN2013/Player/hoard_functions.lua
@@ -226,9 +226,9 @@ hordeFunctions["kickRight"] = function(args,client)
 end
 hordeFunctions["gotoPose"] = function(args, client)
 	-- set the wcm values to the x,y,a from the args
-	print("The args for gotoPose: " .. args .. "\n");
+	print("The args for gotoPose: " .. tostring(args) .. "\n");
 	print("HI");
-	dest = json.decode(args)
+	dest = args
 	print("Dest " .. dest.x);
 	--print("The args more specfied... X" .. args.x .. " Y " .. args.y .. " theta: " .. args.a);
 	wcm.set_horde_gotoPose(vector.new({dest.x, dest.y, dest.a}));
