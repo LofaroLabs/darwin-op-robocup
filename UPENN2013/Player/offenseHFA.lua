6ad5b80faffee39e2fbd44bf6db654fae5fc3720
David <devnull@localhost>
2014-06-06 17:01:13 -0400

    fixed goal you should be closest to

--- UPENN2013/Player/offenseHFA.lua
+++ UPENN2013/Player/offenseHFA.lua
@@ -272,14 +272,16 @@ myMachine = makeHFA("myMachine", makeTransition({
 	[start] =function() print("well i got into start.... idk where i go from here")  return {[0] = kittyOrPassMachine, ["openSpot"] = "openSpot" } end, --gotoPoseFacing,
 	[kittyOrPassMachine] = function() 
 		print("closest to ball value is: " .. tostring(closestToBall())); 
-		if closestToBall()~=1 and not wcm.get_team_isClosestToGoalOffend()==1 then 
+		if closestToBall()~=1 and not wcm.get_team_isClosestToGoalDefend()==1 then 
 			return {[0] = gotoPosition, ["openSpot"] = "openSpot"}
+		elseif closestToBall()~=1 and wcm.get_team_isClosestToGoalDefend()==1 then
+			return safety
 		end 
 		return {[0] = kittyOrPassMachine, ["openSpot"] = "openSpot"} end,
 	[gotoPosition] = function() print("SHOULD BE IN GOTO POSE") 		
 		if(closestToBall()==1) then 
 			return {[0] = kittyOrPassMachine, ["openSpot"] = "openSpot"} 
-		elseif wcm.get_team_isClosestToGoalOffend()==1 then
+		elseif wcm.get_team_isClosestToGoalDefend()==1 then
 			return safety
 		else 
 			return {[0] = gotoPosition, ["openSpot"] = "openSpot"} 
@@ -288,7 +290,7 @@ myMachine = makeHFA("myMachine", makeTransition({
 	[safety] = function()
 		if(closestToBall()==1)	then 
 			return kittyOrPassMachine
-		elseif(wcm.get_team_isClosestToGoalOffend()==1) then
+		elseif(wcm.get_team_isClosestToGoalDefend()==1) then
 			return safety
 		end
 		return gotoPosition
