4eeff96415cf0fa0bc0b4a38b0b8531f1c0ab936
dwicke <devnull@localhost>
2014-06-10 12:31:37 -0400

    added sleep to script

--- UPENN2013/Player/scripts/restartcam.sh
+++ UPENN2013/Player/scripts/restartcam.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #probably need to run as sudo
 echo '1-8' | sudo tee /sys/bus/usb/drivers/usb/unbind
+sleep 2
 echo '1-8' | sudo tee /sys/bus/usb/drivers/usb/bind
-uvcdynctrl -L ../default.gpfl
+sleep 3
+
+uvcdynctrl -L default.gpfl
 
