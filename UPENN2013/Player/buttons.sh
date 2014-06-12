0c10a0edc9e9fbf06b24618f65282cfb15210e27
dwicke <devnull@localhost>
2014-06-12 17:50:36 -0400

    added the calls to the script

--- UPENN2013/Player/buttons.sh
+++ UPENN2013/Player/buttons.sh
@@ -2,12 +2,5 @@
 GODIR=/home/darwin/dev/merc/darwin/UPENN2013/Player
 cd $GODIR
 #screen -dm -L -s /usr/bin/bash -S dcm  
-lua ./run_dcm.lua &
-sleep 5
-#screen -dm -L -s /usr/bin/bash -S cog 
-lua ./run_cognition.lua &
-sleep 5
-
-lua ./gmu_buttons.lua &
-
+lua ./dummy.lua &
 
--- UPENN2013/Player/gmu_buttons.lua
+++ UPENN2013/Player/gmu_buttons.lua
@@ -15,37 +15,27 @@ require('os')
 function update() 
 	if ((Body.get_time() - tButton) > 0.25) then
 		tButton = Body.get_time();
+		
 		if (Body.get_change_role() == 1) then
 			scriptNumber = scriptNumber + 1;
 			print("Incrementing leftbutton script")
 			if     scriptNumber == 0 then 
 				Speak.talk('Ready Aim Fire')
 				-- this is full soccer
-				os.execute("sh runbasic.sh");
-				os.execute("sh scripts/fullsoccer.sh ");
                         elseif scriptNumber == 1 then 
 				Speak.talk('Vision Calibration Mode')
-				-- then we call the vision calibration
-				os.execute("sh runbasic.sh");
-				os.execute("echo 111111 | sudo -S sh scripts/startCalibration.sh")
 			elseif scriptNumber == 2 then Speak.talk('Demo Mode')
-				os.execute("sh runbasic.sh");
-				os.execute("sh scripts/demomode.sh");
 			elseif scriptNumber == 3 then 
 				Speak.talk('Reset Internet Mode')
-				os.execute("echo 111111 | sudo -S sh scripts/resetInternet.sh")
 			elseif scriptNumber == 4 then 
 				Speak.talk('Kitty Soccer Mode')
-				os.execute("sh scripts/kittyMode.sh");
 			elseif scriptNumber == 5 then 
 				Speak.talk('Reset Camera')
-				os.execute("echo 111111 | sudo -S sh scripts/restartcam.sh")
 			elseif scriptNumber == 6 then 
 				Speak.talk('Reset Darwin')
-				os.execute("sh runbasic.sh")
 			elseif scriptNumber == 7 then Speak.talk('Nothing')
 			else   scriptNumber = 0; -- May be able to remove this and change last elseif to else
-		end
+			end
 
 			--if we want to just run main then press the role button
 			--Speak.talk('Changed role is one')
@@ -63,6 +53,39 @@ function update()
 			print("Executing selected button script")
 			-- os.execute("sh middlebutton.sh ".. tostring(middleCount % 2))
 			middleCount = middleCount + 1;
+
+
+			if     scriptNumber == 0 then
+                                Speak.talk('Ready Aim Fire')
+                                -- this is full soccer
+                                os.execute("sh runbasic.sh");
+                                os.execute("sh scripts/fullsoccer.sh ");
+                        elseif scriptNumber == 1 then
+                                Speak.talk('Vision Calibration Mode')
+                                -- then we call the vision calibration
+                                
+                                os.execute("echo 111111 | sudo -S sh scripts/startCalibration.sh")
+                        elseif scriptNumber == 2 then Speak.talk('Demo Mode')
+                                os.execute("sh runbasic.sh");
+                                os.execute("sh scripts/demomode.sh");
+                        elseif scriptNumber == 3 then
+                                Speak.talk('Reset Internet Mode')
+                                os.execute("echo 111111 | sudo -S sh scripts/resetInternet.sh")
+                        elseif scriptNumber == 4 then
+                                Speak.talk('Kitty Soccer Mode')
+                                os.execute("sh scripts/kittyMode.sh");
+                        elseif scriptNumber == 5 then
+                                Speak.talk('Reset Camera')
+                                os.execute("echo 111111 | sudo -S sh scripts/restartcam.sh")
+                        elseif scriptNumber == 6 then
+                                Speak.talk('Reset Darwin')
+                                os.execute("sh runbasic.sh")
+                        elseif scriptNumber == 7 then Speak.talk('Nothing')
+                        else   scriptNumber = 0; -- May be able to remove this and change last elseif to else
+
+			end
+
+
 		end	
 	end
 
@@ -77,6 +100,8 @@ scriptNumber = 0;	-- Default Mode
 Speak.talk('George Mason Awesome Robo Patriots')
 
 local tDelay = 0.005 * 1E6;
+
+
 while 1 do
 	update();
 	unix.usleep(tDelay);
