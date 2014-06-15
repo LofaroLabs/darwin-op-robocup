3d8bff270f04b998288ad540f129db8d26d45bf0
dwicke <devnull@localhost>
2014-06-14 20:03:18 -0400

    added body and head behaviors for goalie

---
+++ UPENN2013/Player/HeadFSM/GeneralPlayer/headLookBehindAndBall.lua
@@ -0,0 +1,52 @@
+module(..., package.seeall);
+
+require('Body')
+require('vcm')
+require('mcm')
+
+t0 = 0;
+timeout = 1.0;
+
+function entry()
+  pitchBias =  mcm.get_headPitchBias();--robot specific head bias
+
+  print(_NAME.." entry");
+
+  t0 = Body.get_time();
+
+  -- set head to default position
+  local yaw = Config.head.yawMax;
+  local pitch =  27*math.pi/180;-- 22 is MAX DONOT GO HIGH (thank you chau)
+  Body.set_head_command({yaw, pitch});
+
+  -- continuously switch cameras
+  vcm.set_camera_command(-1);
+end
+yawMax = Config.head.yawMax;
+lookBall = false
+function update()
+  if(Body.get_time()-t0 > 3) then
+	if lookBall then -- only switch which shoulder we look over after we've looked at a ball
+		yawMax = -1*yawMax;
+  	end
+	lookBall = (not lookBall)
+	t0 = Body.get_time();
+  end
+  pitch = -27*math.pi/180;-- 22 is MAX DONOT GO HIGH (thank you chau)
+  yaw = yawMax;
+  if lookBall then
+	ballGlobal= {};
+        ballGlobal.x = wcm.get_ballGlobal_x();
+        ballGlobal.y = wcm.get_ballGlobal_y();
+	ballRelative  = util.pose_relative(endPosition, {pose.x, pose.y, pose.a});
+	ballx = ballRelative[1];
+  	bally = ballRelative[2];
+
+	yaw, pitch =
+        HeadTransform.ikineCam(ballx, bally, .1, 0);
+  end
+  Body.set_head_command({yaw, pitch});
+end
+
+function exit()
+end
