54d7065e01187961b10ae1f2f66206e016631553
dwicke <devnull@localhost>
2014-07-12 21:35:01 -0400

    made it so that gcm will not get a different side of field

--- UPENN2013/Player/Lib/OPGameControl.lua
+++ UPENN2013/Player/Lib/OPGameControl.lua
@@ -250,8 +250,8 @@ function update_shm()
   gcm.set_game_time_remaining(timeRemaining);
   gcm.set_game_last_update(lastUpdate);
 
-  --gcm.set_team_number(teamNumber);
-  gcm.set_team_color(teamColor);
+  gcm.set_team_number(teamNumber);
+  --gcm.set_team_color(teamColor);
 end
 
 function exit()
