3844bf0f654682c399ac1dcf95918bbd2243850b
dwicke <devnull@localhost>
2014-07-20 09:54:17 -0300

    fixed horde start script

--- UPENN2013/Player/runhorde.sh
+++ UPENN2013/Player/runhorde.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #sh runbasic.sh
-export CLASSPATH=.:/home/darwin/horde/
+export CLASSPATH=.:/home/darwin/dev/merc/darwin/horde/
 sleep 4
 screen -dm -s /usr/bin/bash -S dar java sim.app.horde.scenarios.robot.darwin.DarwinHorde soccer_localhost.arena
