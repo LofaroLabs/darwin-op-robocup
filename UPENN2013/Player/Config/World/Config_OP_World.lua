f86b36760b01a53888e4a980b836a0ed9afac5aa
dwicke <devnull@localhost>
2014-07-21 08:25:35 -0300

    fixed goal location

--- UPENN2013/Player/Config/World/Config_OP_World.lua
+++ UPENN2013/Player/Config/World/Config_OP_World.lua
@@ -39,15 +39,15 @@ world.Lcorner[4]={-2.74,-1.80};
 world.Lcorner[5]={0,1.80};
 world.Lcorner[6]={0,-1.80};
 --Penalty box edge --UPDATED SECTION
-world.Lcorner[7]={-2.26,.82};--226cm,82cm 2.26 is from center field to penalty box first piece of tape
-world.Lcorner[8]={-2.26,-.82};
-world.Lcorner[9]={2.26,.82};
-world.Lcorner[10]={2.26,-.82};
+world.Lcorner[7]={-3.9,.82};--226cm,82cm 2.26 is from center field to penalty box first piece of tape
+world.Lcorner[8]={-3.9,-.82};
+world.Lcorner[9]={3.9,.82};
+world.Lcorner[10]={3.9,-.82};
 --Penalty box T edge --UPDATED SECTION -- 2.74 is the field edge
-world.Lcorner[11]={2.74,.82};
-world.Lcorner[12]={2.74,-.82};
-world.Lcorner[13]={-2.74,.82};
-world.Lcorner[14]={-2.74,-.82};
+world.Lcorner[11]={4.5,.82};
+world.Lcorner[12]={4.5,-.82};
+world.Lcorner[13]={-4.5,.82};
+world.Lcorner[14]={-4.5,-.82};
 --Center circle junction --UPDATED SECTION
 world.Lcorner[15]={0,0.56};
 world.Lcorner[16]={0,-0.56};
