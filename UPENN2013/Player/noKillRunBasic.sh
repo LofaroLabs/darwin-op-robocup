690a9bff40ac5bddffd409fb7b0497814c8b802d
David <devnull@localhost>
2014-10-22 03:55:57 -0400

    cs 485 cs485 485

--- UPENN2013/Player/noKillRunBasic.sh
+++
@@ -1,8 +0,0 @@
-#asssumg dcm on start up
-sleep 1
-screen -dm -s /usr/bin/bash -S cog lua ./run_cognition.lua
-sleep 3
-screen -dm -s /usr/bin/bash -S hor lua ./hoard_connection.lua
-echo `screen -ls`
-
-
--- UPENN2013/Player/toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-until screen -S tog lua toggle_features.lua; do
-	echo "hi"
-done
-echo "HI"
-
-
-lua toggle_off.lua 
